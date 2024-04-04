2c0e4df72633d2d35d325314b1492a3066b32104
Koshitha Amarakoon <koshithaa@n-able.biz>
2024-04-04 14:45:26 +0530

    Updated model training block

--- ml_demo/data_exporters/save_model.py
+++ ml_demo/data_exporters/save_model.py
@@ -1,6 +1,9 @@
 from mage_ai.io.file import FileIO
 from pandas import DataFrame
 import joblib
+import pandas as pd
+from sklearn.cluster import KMeans
+
 
 if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
@@ -22,18 +25,9 @@ def export_data(df: DataFrame, **kwargs) -> None:
     # Re-Train K means model with k=5
     kmeans_model_new = KMeans(n_clusters = 5,init='k-means++',max_iter=400,random_state=42)
 
-    new_predictions = kmeans_model_loaded.predict(dara[['products_purchased','complains','money_spent']])
- 
-    # Count the number of data points in each cluster
-    cluster_counts = pd.Series(new_predictions).value_counts()
+    # Save the trained K-means model
+    joblib.dump(kmeans_model_new, 'kmeans_model.pkl')
 
-    # Print the counts of data points in each cluster
-    print("Cluster Counts:")
-    print(cluster_counts)
+    # Load the saved K-means model
+    kmeans_model_loaded = joblib.load('kmeans_model.pkl')
 
-    # Calculate the average spending behavior for each cluster
-    cluster_spending = new_data.groupby(new_predictions)['money_spent'].mean()
-
-    # Print the average spending behavior for each cluster
-    print("\nAverage Spending Behavior for Each Cluster:")
-    print(cluster_spending)
