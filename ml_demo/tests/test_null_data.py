085406de9471734fa6363c18ed1a9bdb5e58460f
Koshitha Amarakoon <koshithaa@n-able.biz>
2024-04-17 08:50:54 +0530

    Updated testcase

--- ml_demo/tests/test_null_data.py
+++ ml_demo/tests/test_null_data.py
@@ -2,28 +2,29 @@ import unittest
 import pandas as pd
 import os,sys
 
+sys.path.append('../')  
 from ml_demo.transformers.check_null import check_null
 
 class TestCheckNullFunction(unittest.TestCase):
 
     def test_check_null(self):
 
-        # test 02 -> test with empty df
+        # test 01 -> test with empty df
         df = pd.DataFrame()
         expected_result = pd.DataFrame(index=df.index, columns=df.columns, data=False)
         self.assertTrue(check_null(df).equals(expected_result))
 
         # test 02 -> test with no null values
-        # read data with no null values
+        # read data without null values
         df_1 =  pd.read_csv('ml_demo/tests/data/data_without_null.csv')
         self.assertTrue(check_null(df_1).equals(df_1))
 
-
-    
-
-
-
-
+        #test 03 -> test with null values
+        # Create a DataFrame with null values
+        df_2 = pd.pd.read_csv('ml_demo/tests/data/data_with_null.csv')
+        result = check_null(df_2) 
+        # Assert that the function returns None
+        self.assertIsNone(result)
 
 
 if __name__ == '__main__':
