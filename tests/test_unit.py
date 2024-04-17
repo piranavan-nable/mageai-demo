2352b054567382e42b9a6fbb960698a5f7dda600
Dinithi Athukorala <dinithia@n-able.biz>
2024-04-17 12:29:28 +0530

    added unit case1

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,12 +1,14 @@
 import unittest
+from demo1.data_loaders.load_data_missing_hdl import load_data_from_file
 
-class TestCI(unittest.TestCase):
-    def test_echo(self):
-        # Simulate the echo command
-        output = "Hello, world!"
-        
-        # Assert that the output is as expected
-        self.assertEqual(output, "Hello, worl!")
+
+class ExamplePipelineTest(unittest.TestCase):
+
+    def test_handle_missing_values_data_loading(self):
+       
+        result = load_data_from_file()
+        self.assertIsNotNone(result)
+  
 
 if __name__ == '__main__':
     unittest.main()
